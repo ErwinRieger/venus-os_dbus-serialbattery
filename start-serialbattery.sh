e47ced4bb5545ae3d1fc21cb9ef0006da697bfd2
ErwinRieger <erwin.rieger@ibrieger.de>
2025-08-11 17:34:28 +0200

    Fix SetupHelper install.

--- start-serialbattery.sh
+++ start-serialbattery.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-#
 
 . /opt/victronenergy/serial-starter/run-service.sh
 
-# app=$(dirname $0)/dbus-serialbattery.py
-
-# start -x -s $tty
-app="python /opt/victronenergy/dbus-serialbattery/dbus-serialbattery.py"
+app="python /data/venus-os_dbus-serialbattery/dbus-serialbattery.py"
 args="/dev/$tty"
 start $args
